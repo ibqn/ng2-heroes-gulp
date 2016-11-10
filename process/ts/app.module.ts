3191f186b23c8cfcee6cdcc336d66e6e0fab2d16
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2016-11-10 18:40:27 +0100

    add http module as dependency

--- process/ts/app.module.ts
+++ process/ts/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }              from '@angular/core';
 import { BrowserModule }         from '@angular/platform-browser';
 import { FormsModule}            from '@angular/forms';
+import { HttpModule }            from '@angular/http';
 
 import { AppComponent }          from './app.component';
 import { HeroDetailComponent }   from './hero-detail.component';
@@ -16,6 +17,7 @@ import { AppRoutingModule }   from './app-routing.module';
     imports: [
         BrowserModule,
         FormsModule,
+        HttpModule,
         AppRoutingModule,
     ],
     providers: [HeroService],

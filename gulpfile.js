412b562cab1383ea1c72c80c543db414087f78c8
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2016-11-18 01:37:21 +0100

    rewrite parts of the gulp file with arrow functions, wait until exec completes, run on windows as well

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,19 @@
 //jshint esversion: 6
 'use strict';
 
-var gulp          = require('gulp'),
-    gutil         = require('gulp-util'),
-    sourcemaps    = require('gulp-sourcemaps'),
-    filter        = require('gulp-filter'),
-    gulpif        = require('gulp-if'),
-    rename        = require("gulp-rename"),
-    typescript    = require('gulp-typescript');
+const gulp          = require('gulp'),
+      gutil         = require('gulp-util'),
+      sourcemaps    = require('gulp-sourcemaps'),
+      filter        = require('gulp-filter'),
+      gulpif        = require('gulp-if'),
+      rename        = require("gulp-rename"),
+      typescript    = require('gulp-typescript');
 
-var fs   = require('fs'),
-    path = require('path'),
-    url  = require('url');
+const fs   = require('fs'),
+      path = require('path'),
+      url  = require('url');
 
-var exec = require('child_process').exec;
+const exec = require('child_process').exec;
 
 var defaultFile = "index.html";
 
@@ -24,7 +24,9 @@ var tscConfig = require('./tsconfig.json');
 
 // gulp should be called like this :
 // $ gulp --type aot
-var isProd = gutil.env.type === 'prod' || gutil.env.type === 'aot';
+const isProd = gutil.env.type === 'prod' || gutil.env.type === 'aot';
+
+const isWin = /^win/.test(process.platform);
 
 
 var sourcesPath = 'process/';
@@ -46,12 +48,12 @@ var targets = {
 };
 
 
-gulp.task('copylibs', function() {
+gulp.task('copylibs', () => {
     if (isProd) {
         return gulp.src([
             'core-js/client/shim',
             'zone.js/dist/zone',
-        ].map(function(i) { return 'node_modules/' + i + '.min.js*'; }))
+        ].map(i => { return 'node_modules/' + i + '.min.js*'; }))
         .pipe(gulp.dest(targets.js));
     }
 
@@ -66,7 +68,7 @@ gulp.task('copylibs', function() {
         'router',
         'forms',
         'upgrade',
-    ].map(function(i) {
+    ].map(i => {
         return 'node_modules/@angular/' + i + '/bundles/' + i + '.umd.js*';
     }))
     .pipe(gulp.dest(targets.js + 'angular'));
@@ -84,7 +86,7 @@ gulp.task('copylibs', function() {
         'zone.js/dist/zone',
         'reflect-metadata/Reflect',
         'systemjs/dist/system.src',
-    ].map(function(i) { return 'node_modules/' + i + '.js*'; }))
+    ].map(i => { return 'node_modules/' + i + '.js*'; }))
     .pipe(gulp.dest(targets.js));
 
     return gulp.src([
@@ -94,7 +96,7 @@ gulp.task('copylibs', function() {
 });
 
 
-gulp.task('ts', function() {
+gulp.task('ts', () => {
     // filter main-aot.ts file in development
     var f = filter(['**', '!**/main-aot.ts']);
 
@@ -109,61 +111,61 @@ gulp.task('ts', function() {
 });
 
 
-gulp.task('css', function() {
+gulp.task('css', () => {
     return gulp.src(sources.css)
     .pipe(gulp.dest(targets.css))
     .pipe(browserSync.stream());
 });
 
 
-gulp.task('html', function() {
-    var f = filter([
+gulp.task('html', () => {
+    let fltr = filter([
         '**/index' + (isProd? '-aot' : '') + '.html'
     ]);
 
     gulp.src(sources.html)
-    .pipe(f)
+    .pipe(fltr)
     .pipe(rename({basename: 'index'}))
     .pipe(gulp.dest(targets.html));
 
-    var f = filter(['**', '!**/index*.html']);
+    fltr = filter(['**', '!**/index*.html']);
 
     return gulp.src(sources.html)
-    .pipe(f)
+    .pipe(fltr)
     .pipe(gulp.dest(targets.html));
 });
 
-var run_proc = function(cmd) {
-    var proc = exec(cmd);
-    proc.stderr.on('data', function(data) {
-        process.stdout.write(data);
-    });
 
-    proc.stdout.on('data', function(data) {
-        process.stdout.write(data);
-    });
-
-    proc.on('end', function() {
-        process.exit()
+let run_proc = (cmd, cb) => {
+    let proc = exec(cmd, (err, stdout, stderr) => {
+        process.stdout.write(stdout);
+        process.stdout.write(stderr);
+        cb(err);
     });
 };
 
-gulp.task('ngc', ['css', 'html', 'ts'], function() {
-    if (isProd) {
-        var cmd  = 'node_modules/.bin/ngc -p tsconfig-aot.json';
-        return run_proc(cmd);
+
+gulp.task('ngc', ['css', 'html', 'ts'], cb => {
+    if (!isProd) return;
+    let cmd  = 'node_modules/.bin/ngc -p tsconfig-aot.json';
+    if (isWin) {
+        cmd  = '"node_modules/.bin/ngc" -p tsconfig-aot.json';
     }
+    return run_proc(cmd, cb);
 });
 
 
-gulp.task('rollup', ['ngc'], function() {
-    if (isProd) {
-        var cmd  = 'node_modules/.bin/rollup -c rollup.config.js';
-        return run_proc(cmd);
+gulp.task('rollup', ['ngc'], cb => {
+    if (!isProd) return;
+    var cmd  = 'node_modules/.bin/rollup -c rollup.config.js';
+    if (isWin) {
+        cmd  = '"node_modules/.bin/rollup" -c rollup.config.js';
     }
+    return run_proc(cmd, cb);
 });
 
-gulp.task('watch', function() {
+
+gulp.task('watch', () => {
     gulp.watch(sources.css, ['css'])
         .on('change', browserSync.reload);
     gulp.watch(sources.html, ['html'])
@@ -176,31 +178,31 @@ gulp.task('watch', function() {
 
 
 gulp.task('browser-sync', [
-        'copylibs',
-        'ts',
-        'html',
-        'css',
-    ], function() {
-        browserSync.init({
-            server: {
-                baseDir: targets.html,
+    'copylibs',
+    'ts',
+    'html',
+    'css',
+], () => {
+    browserSync.init({
+        server: {
+            baseDir: targets.html,
+        },
+        // Middleware for serving Single Page Applications (SPA)
+        middleware: [
+            (req, res, next) => {
+                let fileName = url.parse(req.url);
+                fileName = fileName.href.split(fileName.search).join("");
+                let fileExists = fs.existsSync(targets.html + fileName);
+                if (!fileExists && fileName.indexOf("browser-sync-client") < 0) {
+                    req.url = "/" + defaultFile;
+                }
+                return next();
             },
-            // Middleware for serving Single Page Applications (SPA)
-            middleware: [
-                function (req, res, next) {
-                    var fileName = url.parse(req.url);
-                    fileName = fileName.href.split(fileName.search).join("");
-                    var fileExists = fs.existsSync(targets.html + fileName);
-                    if (!fileExists && fileName.indexOf("browser-sync-client") < 0) {
-                        req.url = "/" + defaultFile;
-                    }
-                    return next();
-                },
-            ],
-            port: 8080,
-            reloadDelay: 300,
-            reloadDebounce: 500
-        });
+        ],
+        port: 8080,
+        reloadDelay: 300,
+        reloadDebounce: 500
+    });
 });
 
 

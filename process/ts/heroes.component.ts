7888b15f15372489dda52f134c02bafdd00748a7
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2016-11-20 15:59:18 +0100

    change identation

--- process/ts/heroes.component.ts
+++ process/ts/heroes.component.ts
@@ -39,19 +39,19 @@ export class HeroesComponent implements OnInit {
         name = name.trim();
         if (!name) return;
         this.heroService.create(name)
-            .then(hero => {
-                this.heroes.push(hero);
-                this.selectedHero = null;
-            });
+        .then(hero => {
+            this.heroes.push(hero);
+            this.selectedHero = null;
+        });
     }
 
     delete(hero: Hero): void {
         this.heroService.delete(hero.id)
-            .then(() => {
-                this.heroes = this.heroes.filter(h => h !== hero);
-                if (this.selectedHero === hero) {
-                    this.selectedHero = null;
-                }
-            });
+        .then(() => {
+            this.heroes = this.heroes.filter(h => h !== hero);
+            if (this.selectedHero === hero) {
+                this.selectedHero = null;
+            }
+        });
     }
 }
